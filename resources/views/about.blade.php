6514d042d1987f1df9de63832fe34301495067be
= <=>
2022-02-13 20:59:57 +0100

    web route, navigation blade updated. about and contact blade created

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h1>About</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dolore voluptas a eos odit id aliquid. Totam omnis quasi aliquam ea culpa. Quo labore ducimus voluptatem aspernatur quibusdam repudiandae vel voluptatibus earum hic est quod ut corrupti velit laudantium eligendi, unde consectetur veritatis veniam animi molestiae rerum debitis! Quia fuga voluptate hic rem voluptates aperiam ut, ipsum, ipsam architecto consequatur quaerat totam itaque veniam? Id, dolorum nesciunt iure sint, aspernatur dicta quidem fuga perferendis voluptas magnam illum atque praesentium quis! Ipsam quisquam assumenda repellat. Iure itaque eaque, minus laborum dolorum modi officiis, dolor enim aperiam reprehenderit, fugiat reiciendis veniam minima tenetur? Ut suscipit labore deleniti dolores cumque tempora at praesentium facere ducimus, natus quam excepturi atque, ratione culpa, sed voluptatem. Officiis dicta dolorum eum aut, sapiente, nam quibusdam fugit iure dolores totam quis minus odio enim voluptatibus vel voluptate! Cupiditate delectus molestias cum eius corrupti quidem repellat facere veritatis voluptate assumenda. Quo placeat iure neque, consequatur voluptatibus officiis suscipit quia reprehenderit. Placeat vitae labore temporibus quasi eos, corporis deleniti, sunt sit, consequatur ducimus dolorem sapiente sint exercitationem ab? Commodi sit accusantium hic debitis recusandae. Aliquid obcaecati tempore distinctio officiis delectus animi porro et necessitatibus dolor harum ratione esse libero, aliquam deserunt cupiditate quasi reiciendis blanditiis. Rem ab ipsa corrupti? Nostrum reprehenderit qui, est tempore quidem voluptatem quia consectetur ea eum voluptatibus culpa quis quaerat. Laborum quae officiis ipsum hic et aperiam id consequuntur totam impedit dolores. Et eveniet placeat ex tempore quod cupiditate esse eum. Aliquam error asperiores consequuntur hic excepturi. Eaque laudantium facilis, molestiae suscipit numquam ex laboriosam facere veniam amet maxime quisquam eligendi odit aliquam unde, error consequatur, iure enim velit cum beatae ea earum. Consectetur eaque dicta accusamus aliquid esse doloribus officia deleniti quod dolorem rerum id placeat deserunt cumque aspernatur labore a illum expedita molestiae, ipsum debitis ullam nostrum nulla quam tempore? Error, labore corporis dignissimos quidem, voluptatum fugiat voluptas sunt est nostrum tempore repudiandae, magni quaerat illo deserunt esse dolorum. Voluptates aliquam ratione quos cum accusamus nihil placeat, animi tempora quia distinctio, et sapiente voluptas voluptate dolor! Temporibus accusantium adipisci porro dolorum! Deleniti veniam officiis maiores, vero a ipsum perferendis aperiam, molestiae numquam quod saepe ullam tempora totam facere consectetur delectus inventore voluptates corrupti. Adipisci accusamus molestias laboriosam et tempora numquam eius, labore dignissimos, dolor maxime repellendus placeat, voluptatem vitae iusto tempore! Nulla voluptas maxime nemo fugiat unde labore iusto impedit eveniet, culpa possimus exercitationem.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,16 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('contact')" :active="request()->routeIs('contact')">
+                        {{ __('Contact') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('about')" :active="request()->routeIs('about')">
+                        {{ __('About') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -65,6 +75,16 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('contact')" :active="request()->routeIs('contact')">
+                {{ __('contact') }}
+            </x-responsive-nav-link>
+        </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('about')" :active="request()->routeIs('about')">
+                {{ __('about') }}
             </x-responsive-nav-link>
         </div>
 
